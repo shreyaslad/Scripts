e4195da3a442ac61e2508c9869e37a5d20814942
Shreyas Lad <shadowtemplates@gmail.com>
2018-08-10 09:58:42 -0700

    Add files via upload

---
+++ helper/README.md
@@ -0,0 +1,13 @@
+# Helper Tool
+This is just a simple CLI tool for actually remembering where the .sh files are.
+
+## Installation
+Coming Soon
+`npm link`
+All done!
+
+## License
+GPL-3.0
+
+## Author
+Shreyas Lad
---
+++ helper/cli.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+console.log("\nWelcome to the helper tool for remembering where all your scripts are.\n");
+console.log("Global Directory Instructions:");
+console.log("On Mac, move your .sh files to the folder /usr/local/bin and then you can just type in the filename");
+console.log("On Windows, type 'path' into the search bar, click 'Edit the system environment variables', click the 'Environment Variables' button, then click 'New', and type in the directory where these files are.\n\n");
+console.log("1. File Options");
+console.log("   - File Name: movepls.sh");
