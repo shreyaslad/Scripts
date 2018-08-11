d968feaa8b3637d63c8b2deb149a4ab080e8fb74
Shreyas Lad <shadowtemplates@gmail.com>
2018-08-10 22:59:19 -0700

    Added shutdown_helper

--- helper/cli.js
+++ helper/cli.js
@@ -6,3 +6,5 @@ console.log("On Mac, move your .sh files to the folder /usr/local/bin and then y
 console.log("On Windows, type 'path' into the search bar, click 'Edit the system environment variables', click the 'Environment Variables' button, then click 'New', and type in the directory where these files are.\n\n");
 console.log("1. File Options");
 console.log("   - File Name: movepls.sh");
+console.log("2. Shudown Help");
+console.log("   - File Name: shutdown_helper.sh");
