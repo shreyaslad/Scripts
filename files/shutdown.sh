e141ffd0ccf8fe5bcc1f31ecc06f4fd9707b9b90
penetratingshot <shadowtemplates@gmail.com>
2018-09-09 00:45:14 -0700

    rewrote everything, some logic added, secondary logic is basic shell

--- files/shutdown.sh
+++ files/shutdown.sh
@@ -18,31 +18,64 @@ LIGHTCYAN='\033[1;36m'
 WHITE='\033[1;37m'
 NC='\033[0m'
 
-printf "\nShutdown\n"
-printf "Detecting Operating System...\n"
-if [[ "$OSTYPE" == "linux-gnu" ]]
+printf "${RED}\nShutdown\n${NC}"
+printf "You are running ${OSTYPE}!\n\n"
+
+echo "Here are some options for managing your computer..."
+if [ "$OSTYPE" == "darwin*" ]
 then
-    echo "You're runnning Linux-GNU."
-elif [[ "$OSTYPE" == "darwin"* ]]
-then
-    echo "Type in the time (min) you like the shutdown delay to be (if no delay, press [ENTER]), followed by [ENTER]:"
-    read delay
-    if [ -z "$1" ]
+    echo "Enter the number associated with the desired option, followed by [ENTER]:"
+    read option
+    echo "1. Shutdown"
+    echo "2. Reboot"
+    echo "3. Logout"
+    echo "4. Halt"
+    if [ "$option" == "1" ]
     then
-        echo "No time supplied, assuming immediate."
-	echo "Shutting down...
-	shutdown -h now
+        echo "Enter the number associated with the desired option, followed by [ENTER]:"
+        read option2
+        echo "1. Shutdown Now"
+        echo "2. Shutdown with Delay"
+        echo "3. Shutdown with Date"
+        echo "4. Shutdown with Date and Delay"
+        echo "5. Abort Shutdown"
+        if [ "$option2" == "1" ]
+        then
+            # hi
+        elif [ "$option2" == "2" ]
+        then
+            # hi
+        elif [ "$option" == "3" ]
+        then
+            # hi
+        elif [ "$option" == "4" ]
+        then
+            # hi
+        elif [ "$option" == "4" ]
+        then
+            # hi
+        else
+            echo "${RED}Fatal: ${NC}The option you've entered, ${option2}, is not on the list."
+    elif [ "$OPTION" == "2" ]
+    then
+        echo "Enter the number associated with the desired option, followed by [ENTER]:"
+        read option2
+        echo "1. Reboot Now"
+        echo "2. Reboot with Delay"
+        echo "3. Reboot with Date"
+        echo "4. Reboot with Date and Delay"
+        echo "5. Abort Reboot"
+    elif [ "$OPTION" == "3" ]
+    then
+        echo "Logging out..."
+        tell application "System Events" to log out
     else
-	echo "Shutting down in ${delay} minutes."
-	shutdown -h +${delay}i	
+        echo "${RED}Fatal: ${NC}The option you've entered, ${option}, is not on the list."
     fi
-elif [[ "$OSTYPE" == "cygwin" ]]
+elif [ "$OSTYPE" == "msys" ]
 then
-    echo "You're running a POSIX layer, emulated Linux on Windows."
-elif [[ "$OSTYPE" == "msys" ]]
-then
-  echo "You're running Windows."
-elif [[ "$OSTYPE" == "freebsd"* ]]
-then
-    echo "You're running Berkely Unix"
+    echo "no"
+else
+    echo "You are running an unsupported operating system... ${RED}terminating${NC}"
+    exit;
 fi
