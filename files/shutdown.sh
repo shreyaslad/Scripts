c8a172d43947b5843edbbad87e1efcf2d43d6c6d
penetratingshot <shadowtemplates@gmail.com>
2018-09-09 00:59:43 -0700

    fixed error by basically deleting everything and restarting

--- files/shutdown.sh
+++ files/shutdown.sh
@@ -18,64 +18,46 @@ LIGHTCYAN='\033[1;36m'
 WHITE='\033[1;37m'
 NC='\033[0m'
 
-printf "${RED}\nShutdown\n${NC}"
-printf "You are running ${OSTYPE}!\n\n"
+
+printf "${RED}\nShutdown${NC}\n"
+printf "${BLUE}hi there${NC}\n"
 
 echo "Here are some options for managing your computer..."
-if [ "$OSTYPE" == "darwin*" ]
-then
     echo "Enter the number associated with the desired option, followed by [ENTER]:"
-    read option
+    
     echo "1. Shutdown"
     echo "2. Reboot"
     echo "3. Logout"
     echo "4. Halt"
-    if [ "$option" == "1" ]
+    read option
+    if [ "$option" = "1" ]
     then
         echo "Enter the number associated with the desired option, followed by [ENTER]:"
-        read option2
+        
         echo "1. Shutdown Now"
         echo "2. Shutdown with Delay"
         echo "3. Shutdown with Date"
         echo "4. Shutdown with Date and Delay"
         echo "5. Abort Shutdown"
-        if [ "$option2" == "1" ]
-        then
-            # hi
-        elif [ "$option2" == "2" ]
-        then
-            # hi
-        elif [ "$option" == "3" ]
-        then
-            # hi
-        elif [ "$option" == "4" ]
-        then
-            # hi
-        elif [ "$option" == "4" ]
-        then
-            # hi
-        else
-            echo "${RED}Fatal: ${NC}The option you've entered, ${option2}, is not on the list."
+        read option2
     elif [ "$OPTION" == "2" ]
     then
         echo "Enter the number associated with the desired option, followed by [ENTER]:"
-        read option2
+        
         echo "1. Reboot Now"
         echo "2. Reboot with Delay"
         echo "3. Reboot with Date"
         echo "4. Reboot with Date and Delay"
         echo "5. Abort Reboot"
+        read option3
     elif [ "$OPTION" == "3" ]
     then
         echo "Logging out..."
         tell application "System Events" to log out
+    elif [ "$OPTION" == "4" ]
+    then
+        echo "Halting any shutdown ot logout processes..."
+        sudo halt
     else
         echo "${RED}Fatal: ${NC}The option you've entered, ${option}, is not on the list."
     fi
-elif [ "$OSTYPE" == "msys" ]
-then
-    echo "no"
-else
-    echo "You are running an unsupported operating system... ${RED}terminating${NC}"
-    exit;
-fi
