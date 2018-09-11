a2bd8f7b8a3a52d98d8de6cd2253e543512fd3a4
penetratingshot <shadowtemplates@gmail.com>
2018-09-10 20:13:58 -0700

    added function for movepls.sh, easy for ~./bashrc

--- files/shutdown.sh
+++ files/shutdown.sh
@@ -18,126 +18,46 @@ LIGHTCYAN='\033[1;36m'
 WHITE='\033[1;37m'
 NC='\033[0m'
 
-testprompt() {
-if [ "$OSTYPE" = "darwin"* ]
-then
-    #hi
-elif [ "$OSTYPE" = "linux-gnu" ]
-then
-    #hi
-elif [ "$OSTYPE" = "freebsd" ]
-then
-    #hi
-elif [ "$OSTYPE" = "cygwin" ]
-then
-    #hi
-elif [ "$OSTYPE" = "msys"* ]
-then
-    #hi
-else
-    printf "You're running an unsupported operating system...${RED}terminating.${NC}\n\n"
-    exit;
-fi
-
-printf "Here are some options for managing your computer...\n"
-printf "${LIGHTRED}1.${NC} Shutdown\n"
-printf "${LIGHTYELLOW}2.${NC} Reboot\n"
-printf "${LIGHTGREEN}3.${NC} Logout\n"
-printf "${LIGHTBLUE}4.${NC} Halt\n\n"
-printf "Enter the number associated with the option you want, followed by [ENTER]: "
-read option
-if [ "$option" = "1" ]
-then
-    echo "Here are the shutdown options:"
-    printf "${LIGHTRED}1.${NC} Shutdown Now\n"
-    printf "${LIGHTYELLOW}2.${NC} Shutdown with Delay\n"
-    printf "${LIGHTGREEN}3.${NC} Shutdown with Date\n"
-    printf "${LIGHTBLUE}4.${NC} Shutdown with Date and Delay\n"
-    printf "${LIGHTPURPLE}5.${NC} Abort Shutdown\n\n"
-    printf "Enter the number associated with the desired option, followed by [ENTER]: "
-    read option2
-elif [ "$OPTION" == "2" ]
-then
-    echo "Here are the reboot options:"
-    printf "${LIGHTRED}1.${NC} Reboot Now\n"
-    printf "${LIGHTYELLOW}2.${NC} Reboot with Delay\n"
-    printf "${LIGHTGREEN}3.${NC} Reboot with Date\n"
-    printf "${LIGHTBLUE}4.${NC} Reboot with Date and Delay\n"
-    printf "${LIGHTPURPLE}5.${NC} Abort Reboot\n"
-    printf "Enter the number associated with the desired option, followed by [ENTER]: "
-    read option3
-elif [ "$OPTION" == "3" ]
-then
-    printf "Logging out..."
-    tell application "System Events" to log out
-elif [ "$OPTION" == "4" ]
-then
-    printf "Halting any shutdown or logout processes..."
-    sudo halt
-else
-    printf "${RED}Fatal: ${NC}The option you've entered, ${option}, is not on the list.\n\n"
-    testprompt
-fi
-}
-
 printf "${RED}\nShutdown${NC}\n"
 
-if [ "$OSTYPE" = "darwin"* ]
+: 'if [ "$OSTYPE" = "darwin" ]
 then
-    #hi
-elif [ "$OSTYPE" = "linux-gnu" ]
-then
-    #hi
-elif [ "$OSTYPE" = "freebsd" ]
-then
-    #hi
-elif [ "$OSTYPE" = "cygwin" ]
-then
-    #hi
-elif [ "$OSTYPE" = "msys"* ]
-then
-    #hi
-else
-    printf "You're running an unsupported operating system...${RED}terminating.${NC}\n\n"
-    exit;
-fi
-
-printf "Here are some options for managing your computer...\n"
-printf "${LIGHTRED}1.${NC} Shutdown\n"
-printf "${LIGHTYELLOW}2.${NC} Reboot\n"
-printf "${LIGHTGREEN}3.${NC} Logout\n"
-printf "${LIGHTBLUE}4.${NC} Halt\n\n"
-printf "Enter the number associated with the option you want, followed by [ENTER]: "
-read option
-if [ "$option" = "1" ]
-then
-    echo "Here are the shutdown options:"
-    printf "${LIGHTRED}1.${NC} Shutdown Now\n"
-    printf "${LIGHTYELLOW}2.${NC} Shutdown with Delay\n"
-    printf "${LIGHTGREEN}3.${NC} Shutdown with Date\n"
-    printf "${LIGHTBLUE}4.${NC} Shutdown with Date and Delay\n"
-    printf "${LIGHTPURPLE}5.${NC} Abort Shutdown\n\n"
-    printf "Enter the number associated with the desired option, followed by [ENTER]: "
-    read option2
-elif [ "$OPTION" == "2" ]
-then
-    echo "Here are the reboot options:"
-    printf "${LIGHTRED}1.${NC} Reboot Now\n"
-    printf "${LIGHTYELLOW}2.${NC} Reboot with Delay\n"
-    printf "${LIGHTGREEN}3.${NC} Reboot with Date\n"
-    printf "${LIGHTBLUE}4.${NC} Reboot with Date and Delay\n"
-    printf "${LIGHTPURPLE}5.${NC} Abort Reboot\n"
-    printf "Enter the number associated with the desired option, followed by [ENTER]: "
-    read option3
-elif [ "$OPTION" == "3" ]
-then
-    printf "Logging out..."
-    tell application "System Events" to log out
-elif [ "$OPTION" == "4" ]
-then
-    printf "Halting any shutdown ot logout processes..."
-    sudo halt
-else
-    printf "${RED}Fatal: ${NC}The option you've entered, ${option}, is not on the list.\n\n"
-    testprompt
-fi
+    printf "Here are some options for managing your computer...\n"
+    printf "${LIGHTRED}1.${NC} Shutdown\n"
+    printf "${LIGHTYELLOW}2.${NC} Reboot\n"
+    printf "${LIGHTGREEN}3.${NC} Logout\n"
+    printf "${LIGHTBLUE}4.${NC} Halt\n\n"
+    printf "Enter the number associated with the option you want, followed by [ENTER]: "
+    read option
+        if [ "$option" = "1" ]
+        then
+            echo "Here are the shutdown options:"
+            printf "${LIGHTRED}1.${NC} Shutdown Now\n"
+            printf "${LIGHTYELLOW}2.${NC} Shutdown with Delay\n"
+            printf "${LIGHTGREEN}3.${NC} Shutdown with Date\n"
+            printf "${LIGHTBLUE}4.${NC} Shutdown with Date and Delay\n"
+            printf "${LIGHTPURPLE}5.${NC} Abort Shutdown\n\n"
+            printf "Enter the number associated with the desired option, followed by [ENTER]: "
+            read option2
+        elif [ "$OPTION" == "2" ]
+        then
+            echo "Here are the reboot options:"
+            printf "${LIGHTRED}1.${NC} Reboot Now\n"
+            printf "${LIGHTYELLOW}2.${NC} Reboot with Delay\n"
+            printf "${LIGHTGREEN}3.${NC} Reboot with Date\n"
+            printf "${LIGHTBLUE}4.${NC} Reboot with Date and Delay\n"
+            printf "${LIGHTPURPLE}5.${NC} Abort Reboot\n"
+            printf "Enter the number associated with the desired option, followed by [ENTER]: "
+            read option3
+        elif [ "$OPTION" == "3" ]
+        then
+            printf "Logging out..."
+            tell application "System Events" to log out
+        elif [ "$OPTION" == "4" ]
+        then
+            printf "Halting any shutdown ot logout processes..."
+            sudo halt
+        else
+            printf "${RED}Fatal: ${NC}The option you've entered, ${option}, is not on the list.\n\n"
+        fi
+fi'
